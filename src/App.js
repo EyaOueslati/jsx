8b2ec24dc8ed3e4ec25944ae1c1e89d0461bb218
EyaOueslati <eya.kramti.info@gmail.com>
2020-11-13 01:40:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import ImgSrc from './ImageInSrc.jpg';
+import ImgPublic from './ImageInPublic.jpg';
+import './style/style.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border: "solid 1px black",
+      maxWidth: "100vw"}}>
+
+<h1 className="title red">Eya Oueslati</h1>
+
+<br/ >
+
+<img src={ImgSrc} />
+
+<br/ >
+
+<img src={ImgPublic} />
+
+</div>
+
+<video style={{ width:"320", height:"240" }} controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+      
+    </div>
+  );
+}
+
+export default App;
